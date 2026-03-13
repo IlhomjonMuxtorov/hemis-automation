3c61399e481722ce1e1f1ea57eeef26ffa1a52d1
Abbosxon <shamsuddinova@gmail.com>
2026-03-13 12:15:55 +0500

    2-bosqich uchun loglarni yozib borish.

--- main.js
+++ main.js
@@ -50,6 +50,24 @@ const fillPttService = require('./services/fillPttService');
             }
         }
 
+        const editedPPTsList = new Map();
+
+        if (fs.existsSync('logs/edited_ptt.jsonl')) {
+            const lines = fs.readFileSync('logs/edited_ptt.jsonl', 'utf8').split('\n');
+
+            for (const line of lines) {
+                if (!line.trim()) continue;
+
+                const data = JSON.parse(line);
+
+                editedPPTsList.set(data.pttId, {
+                    studentId: data.studentId,
+                    pttId: data.pttId,
+                    pttNumber: data.pttNumber
+                });
+            }
+        }
+
         for (const student of students) {
 
             console.log("-------------");
@@ -57,7 +75,7 @@ const fillPttService = require('./services/fillPttService');
             let pttId = null;
             let pttNumber = null;
 
-            // 1 bosqich
+            // 1-bosqich - Qaydnoma yaratish
             if (!processedStudents.has(student.id)) {
                 const createResult = await createPttService(adminPage, student);
 
@@ -107,29 +125,56 @@ const fillPttService = require('./services/fillPttService');
                 console.log(`1-bosqich tashlab o'tildi, sababi bu talaba uchun qaydnoma allaqachon yaratilgan edi. Talaba IDsi: ${student.id} Qaydnoma IDsi: ${pttId} Qaydnoma raqami: ${pttNumber}`);
             }
 
-            const editResult = await editPttService(adminPage, student, pttId);
-            // const editResult = await editPttService(adminPage, student, 884);
+            // 2-bosqich - Qaydnomaga o'qituvchi biriktirish
+            if (!editedPPTsList.has(pttId)) {
+                const editResult = await editPttService(adminPage, student, pttId);
 
-            if (!editResult.success) {
-                console.log(`PTT yaratilmadi: ${student.id}`);
+                if (!editResult.success) {
+                    console.log(`PTT yaratilmadi: ${student.id}`);
 
-                const log = {
+                    const log = {
+                        studentId: student.id,
+                        reason: editResult.message,
+                        time: new Date().toISOString()
+                    };
+
+                    fs.appendFileSync(
+                        'logs/failed_students_second_step.jsonl',
+                        JSON.stringify(log) + "\n"
+                    );
+
+                    continue;
+                }
+
+                const successLog = {
                     studentId: student.id,
-                    reason: editResult.message,
+                    pttId: pttId,
+                    pttNumber: pttNumber,
                     time: new Date().toISOString()
                 };
 
                 fs.appendFileSync(
-                    'logs/failed_students_second_step.jsonl',
-                    JSON.stringify(log) + "\n"
+                    'logs/edited_ptt.jsonl',
+                    JSON.stringify(successLog) + "\n"
                 );
 
-                continue;
+                editedPPTsList.set(pttId, {
+                    studentId: student.id,
+                    pttId: pttId,
+                    pttNumber: pttNumber
+                });
+
+                console.log(`PTT editResult: ${editResult.message}`);
+            } else {
+                // const existing = editedPPTsList.get(pttId);
+                //
+                // pttId = existing.pttId;
+                // pttNumber = existing.pttNumber;
+
+                console.log(`2-bosqich tashlab o'tildi. Talaba IDsi: ${student.id} Qaydnoma IDsi: ${pttId} Qaydnoma raqami: ${pttNumber}`);
             }
 
-            console.log(`PTT editResult: ${editResult.message}`);
-
-
+            // 3-bosqich - Qaydnomani tasdiqlab, rahbariyatga yuborish.
             // 2 bosqich
             // await checkPttService(approverPage, student.id);
             //
